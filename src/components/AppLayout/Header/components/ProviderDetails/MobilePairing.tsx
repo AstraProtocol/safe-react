975b51209c3d94a50d42af120fc8e3539ab653f2
vien.nguyen2-tiki <vien.nguyen2@tiki.vn>
2023-01-10 13:40:27 +0700

    chore: remove unnecessary information

--- src/components/AppLayout/Header/components/ProviderDetails/MobilePairing.tsx
+++ src/components/AppLayout/Header/components/ProviderDetails/MobilePairing.tsx
@@ -1,11 +1,10 @@
 import { lazy, ReactElement } from 'react'
-import { isPairingSupported } from 'src/logic/wallets/pairing/utils'
 import { wrapInSuspense } from 'src/utils/wrapInSuspense'
 
 const PairingDetails = lazy(() => import('src/components/AppLayout/Header/components/ProviderDetails/PairingDetails'))
 
 const MobilePairing = (props: { vertical?: boolean }): ReactElement | null => {
-  return isPairingSupported() ? wrapInSuspense(<PairingDetails {...props} />) : null
+  return false ? wrapInSuspense(<PairingDetails {...props} />) : null
 }
 
 export default MobilePairing
--- src/components/AppLayout/Sidebar/index.tsx
+++ src/components/AppLayout/Sidebar/index.tsx
@@ -13,7 +13,7 @@ import ListIcon from 'src/components/List/ListIcon'
 import { openCookieBanner } from 'src/logic/cookies/store/actions/openCookieBanner'
 import { loadFromCookie } from 'src/logic/cookies/utils'
 import { COOKIES_KEY, BannerCookiesType, COOKIE_IDS } from 'src/logic/cookies/model/cookie'
-import { background, primaryLite } from 'src/theme/variables'
+import { background } from 'src/theme/variables'
 
 const StyledDivider = styled(Divider)`
   margin: 16px -8px 0;
@@ -29,32 +29,32 @@ const HelpList = styled.div`
   padding: 0 12px;
 `
 
-const HelpCenterLink = styled.a`
-  width: 100%;
-  display: flex;
-  position: relative;
-  box-sizing: border-box;
-  text-align: left;
-  align-items: center;
-  padding: 6px 12px;
-  justify-content: flex-start;
-  text-decoration: none;
-  border-radius: 8px;
+// const HelpCenterLink = styled.a`
+//   width: 100%;
+//   display: flex;
+//   position: relative;
+//   box-sizing: border-box;
+//   text-align: left;
+//   align-items: center;
+//   padding: 6px 12px;
+//   justify-content: flex-start;
+//   text-decoration: none;
+//   border-radius: 8px;
 
-  &:hover {
-    background-color: ${primaryLite};
-  }
-  p {
-    font-family: ${({ theme }) => theme.fonts.fontFamily};
-    font-size: 0.76em;
-    font-weight: 600;
-    line-height: 1.5;
-    letter-spacing: 1px;
-    color: ${({ theme }) => theme.colors.placeHolder};
-    text-transform: uppercase;
-    padding: 0 0 0 4px;
-  }
-`
+//   &:hover {
+//     background-color: ${primaryLite};
+//   }
+//   p {
+//     font-family: ${({ theme }) => theme.fonts.fontFamily};
+//     font-size: 0.76em;
+//     font-weight: 600;
+//     line-height: 1.5;
+//     letter-spacing: 1px;
+//     color: ${({ theme }) => theme.colors.placeHolder};
+//     text-transform: uppercase;
+//     padding: 0 0 0 4px;
+//   }
+// `
 type Props = {
   safeAddress?: string
   safeName?: string
@@ -138,12 +138,12 @@ const Sidebar = ({
             </Track>
           )}
 
-          <Track {...OVERVIEW_EVENTS.HELP_CENTER}>
+          {/* <Track {...OVERVIEW_EVENTS.HELP_CENTER}>
             <HelpCenterLink href="https://help.gnosis-safe.io/en/" target="_blank" title="Help Center of Astra Safe">
               <ListIcon type="question" color="secondary" size="sm" />
               <StyledListItemText>Help Center</StyledListItemText>
             </HelpCenterLink>
-          </Track>
+          </Track> */}
         </HelpList>
       </HelpContainer>
     </>
